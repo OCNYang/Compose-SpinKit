c6d131badf8ad32ae8629dc27db5c3c07b23a585
ocnyang <ocnyang@gmail.com>
2024-03-05 15:27:36 +0800

    fixed;

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("com.github.jitpack:android-example:1.0.1")
 }
 
 publishing {
@@ -59,7 +57,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.ocnyang"
             artifactId = "compose-spinkit"
-            version = "1.0.3"
+            version = "1.0.4"
 
             afterEvaluate {
                 from(components["release"])
